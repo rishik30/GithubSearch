ec58da80af431cecf469aa325264063be6bef6d3
Rishabh Kumar <rishabhk3003@gmail.com>
2017-11-11 06:44:56 +0530

    removed unnecessary code

--- app.js
+++ app.js
@@ -13,24 +13,6 @@ app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-// app.get('/api/callback', (req, res) => {
-// 	const code = req.query.code
-// 	console.log('CODE', code)
-// 	if (code) {
-// 		res.send({
-// 			type: 'login',
-// 			message: 'Successful login',
-// 			data: code
-// 		})
-// 	} else {
-// 		res.send({
-// 			type: 'login',
-// 			message: 'Unsuccessful login',
-// 			data: undefined
-// 		})
-// 	}
-// })
-
 app.listen(port, ()=>{
     console.log(`Listening on port: ${port}`)
 })
--- src/jsx/github.jsx
+++ src/jsx/github.jsx
@@ -7,12 +7,6 @@ export default class GithubProcess extends Component {
 	componentDidMount () {
 		console.log('URL', window.location.search)
 		const code = window.location.search.split('=').pop()
-		// axios({
-		// 	url: `${window.location.origin}/api/callback`,
-		// 	params: {
-		// 		code
-		// 	}
-		// })
 		if (code) {
 			window.location = window.location.origin + '/dashboard'
 			localStorage.session_code = code
